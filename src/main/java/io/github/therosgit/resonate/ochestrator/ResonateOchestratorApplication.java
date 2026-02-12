44cac07a6a59c693caa098a8f5fbadb53826b89b
Thero Thethe <thethepercy@gmail.com>
2026-02-12 20:26:36 +0200

    restructure main package

--- src/main/java/io/github/therosgit/resonate/ochestrator/ResonateOchestratorApplication.java
+++ src/main/java/io/github/therosgit/resonate/ochestrator/ResonateOchestratorApplication.java
@@ -1,4 +1,4 @@
-package io.github.therosgit.resonate_ochestrator;
+package io.github.therosgit.resonate.ochestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/github/therosgit/resonate/ochestrator/ResonateOchestratorApplicationTests.java
+++ src/test/java/io/github/therosgit/resonate/ochestrator/ResonateOchestratorApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.therosgit.resonate_ochestrator;
+package io.github.therosgit.resonate.ochestrator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
