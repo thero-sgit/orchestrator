57ad8f11056f671c96372cc04189c7bf04f67d43
Thero Thethe <thethepercy@gmail.com>
2026-02-12 20:14:08 +0200

    init project

---
+++ src/main/java/io/github/therosgit/resonate_ochestrator/ResonateOchestratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.therosgit.resonate_ochestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResonateOchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResonateOchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/therosgit/resonate_ochestrator/ResonateOchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.therosgit.resonate_ochestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResonateOchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
