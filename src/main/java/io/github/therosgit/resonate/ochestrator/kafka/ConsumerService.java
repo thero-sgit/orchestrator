8b02988c1263379c67e6ed3df0bf35941afdda97
Thero Thethe <thethepercy@gmail.com>
2026-02-15 13:23:08 +0200

    add kafka consumer class: ConsumerService

---
+++ src/main/java/io/github/therosgit/resonate/ochestrator/kafka/ConsumerService.java
@@ -0,0 +1,33 @@
+package io.github.therosgit.resonate.ochestrator.kafka;
+
+import java.util.List;
+
+import org.springframework.kafka.annotation.KafkaListener;
+
+import io.github.therosgit.resonate.ochestrator.domain.Fingerprint;
+import io.github.therosgit.resonate.ochestrator.repository.FingerprintRepository;
+
+public class ConsumerService {
+    private FingerprintRepository repository;
+
+    public ConsumerService(FingerprintRepository repository) {
+        this.repository = repository;
+    }
+
+    @KafkaListener
+    public void consume(FingerprintGeneratedEvent event) {
+        List<Fingerprint> entities = event.fingerprints()
+            .stream()
+            .map(pair -> {
+                Fingerprint fingerprint = new Fingerprint();
+                fingerprint.setSongId(event.songId());
+                fingerprint.setHash(pair.get(0));
+                fingerprint.setOffset(pair.get(1));
+
+                return fingerprint;
+            })
+            .toList();
+
+        repository.saveAll(entities);
+    }
+}
