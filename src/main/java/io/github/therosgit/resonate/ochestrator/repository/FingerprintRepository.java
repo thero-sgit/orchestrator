d16470525a4950520fd15bc23fb03bac1cbdbf84
Thero Thethe <thethepercy@gmail.com>
2026-02-15 12:41:34 +0200

    new repository class: FingerprintRepository

---
+++ src/main/java/io/github/therosgit/resonate/ochestrator/repository/FingerprintRepository.java
@@ -0,0 +1,12 @@
+package io.github.therosgit.resonate.ochestrator.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.therosgit.resonate.ochestrator.domain.Fingerprint;
+
+public interface FingerprintRepository extends JpaRepository<Fingerprint, Long> {
+
+    List<Fingerprint> findByHashIn(List<Integer> hashes);
+}
