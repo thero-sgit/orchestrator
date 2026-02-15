51107fd5a052f50d0fc46fde43ec442eb0285ddd
Thero Thethe <thethepercy@gmail.com>
2026-02-15 12:30:04 +0200

    new domain model: Song

---
+++ src/main/java/io/github/therosgit/resonate/ochestrator/domain/Song.java
@@ -0,0 +1,53 @@
+package io.github.therosgit.resonate.ochestrator.domain;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "songs")
+public class Song {
+    @Id
+    private UUID id;
+
+    private String title;
+
+    private String s3key;
+
+    private Instant uploadedAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getS3key() {
+        return s3key;
+    }
+
+    public Instant getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setS3key(String s3key) {
+        this.s3key = s3key;
+    }
+
+    public void setUploadedAt(Instant uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }    
+}
