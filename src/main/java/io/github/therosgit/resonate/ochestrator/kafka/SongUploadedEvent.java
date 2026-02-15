2f9a276661ff6987a0ce6d93a48d050e28e4aae9
Thero Thethe <thethepercy@gmail.com>
2026-02-15 12:44:43 +0200

    new kafka event entity class: SongUploadedEvent

---
+++ src/main/java/io/github/therosgit/resonate/ochestrator/kafka/SongUploadedEvent.java
@@ -0,0 +1,10 @@
+package io.github.therosgit.resonate.ochestrator.kafka;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record SongUploadedEvent(
+    UUID songId,
+    String s3key,
+    Instant timeUploaded
+) {}
