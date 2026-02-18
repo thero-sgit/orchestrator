2198dc06bf8b52fc3288e4cb9a7ce8948061d5d7
Thero <pethethcjc025@student.wethinkcode.co.za>
2026-02-18 14:34:35 +0200

    new test: RdsIntegration.fingerprintsTableExists to test if the fingerprints table in auto created

--- src/test/java/io/github/therosgit/resonate/ochestrator/integration/RdsIntegration.java
+++ src/test/java/io/github/therosgit/resonate/ochestrator/integration/RdsIntegration.java
@@ -3,6 +3,7 @@ package io.github.therosgit.resonate.ochestrator.integration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
@@ -18,5 +19,22 @@ public class RdsIntegration {
     @Test
     void connectionEstablished() {
         assertThat(dataSource).isNotNull();
+    }
+
+    @Test
+    void fingerprintsTableExists() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        String sql = """
+            SELECT count(*)\s
+            FROM information_schema.tables\s
+            WHERE table_schema = 'public'
+              AND table_name = ?
+       \s""";
+
+        String tableName = "fingerprints";
+
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, tableName);
+        assertThat(count).as("Table 'fingerprints' should exist in schema 'public'").isGreaterThan(0);
     }
 }
