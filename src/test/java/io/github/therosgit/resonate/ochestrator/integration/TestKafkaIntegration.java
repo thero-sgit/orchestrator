a6b5ca84abfaef5e87e42ef37fc4d772071cf864
Thero <pethethcjc025@student.wethinkcode.co.za>
2026-02-20 13:30:34 +0200

    new test class: TestKafkaIntegration to test ProducerService with LocalStack

---
+++ src/test/java/io/github/therosgit/resonate/ochestrator/integration/TestKafkaIntegration.java
@@ -0,0 +1,76 @@
+package io.github.therosgit.resonate.ochestrator.integration;
+
+import io.github.therosgit.resonate.ochestrator.kafka.ProducerService;
+import io.github.therosgit.resonate.ochestrator.kafka.SongUploadedEvent;
+import io.github.therosgit.resonate.ochestrator.kafka.SongsUploadedConsumer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
+
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
+public class TestKafkaIntegration {
+    @Container
+    static private KafkaContainer kafka = new KafkaContainer("apache/kafka-native:3.8.0");
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", () -> kafka.getBootstrapServers());
+    }
+
+    @Autowired
+    private KafkaTemplate<String, SongUploadedEvent> template;
+
+    @Autowired
+    private SongsUploadedConsumer consumer;
+
+    private ProducerService producerService;
+
+    @BeforeEach
+    void setUp() {
+        producerService = new ProducerService(template);
+    }
+
+    @Test
+    void testSimpleSend() {
+        UUID songId = UUID.randomUUID();
+        String time = Instant.now().toString();
+
+        SongUploadedEvent event = new SongUploadedEvent(songId, "test-s3Key", time);
+        producerService.sendSongUploaded(event);
+
+        try {
+            boolean messageReceived = consumer.getLatch().await(5, TimeUnit.SECONDS);
+
+            await()
+                    .atMost(10, TimeUnit.SECONDS)
+                    .untilAsserted(() -> {
+                        assertThat(consumer.getPayload()).isNotNull();
+
+                        assertThat(consumer.getPayload().songId()).isEqualTo(songId);
+                        assertThat(consumer.getPayload().s3key()).isEqualTo("test-s3Key");
+                        assertThat(consumer.getPayload().timeUploaded()).isEqualTo(time);
+                    });
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
